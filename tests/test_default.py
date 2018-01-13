28efe31a6bd28116149b5538e8af88894cbb8561
paulfantom <pawel@krupa.net.pl>
2018-01-13 18:47:51 +0100

    refactor tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,33 +4,23 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 def test_files(host):
-    present = [
+    files = [
         "/etc/systemd/system/snmp_exporter.service",
         "/opt/snmp_exporter/snmp_exporter",
         "/opt/snmp_exporter/snmp.yml"
     ]
-    if present:
-        for file in present:
-            f = host.file(file)
-            assert f.exists
-            assert f.is_file
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
 
 
 def test_service(host):
-    present = [
-        "snmp_exporter"
-    ]
-    if present:
-        for service in present:
-            s = host.service(service)
-            assert s.is_enabled
-            assert s.is_running
+    s = host.service("snmp_exporter")
+    assert s.is_enabled
+    assert s.is_running
 
 
 def test_socket(host):
-    present = [
-        "tcp://127.0.0.1:9116"
-    ]
-    for socket in present:
-        s = host.socket(socket)
-        assert s.is_listening
+    s = host.socket("tcp://127.0.0.1:9116")
+    assert s.is_listening
